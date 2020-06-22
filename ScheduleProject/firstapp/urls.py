afc3b909dae46e7c44b2cb6090a99e0d6841bf55
Jayanta <jayantasadhu4557@gmail.com>
2020-06-23 01:40:29 +0600

    admin scheduling page complete

--- ScheduleProject/firstapp/urls.py
+++ ScheduleProject/firstapp/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('mainpage/', views.schedulepage, name='schedule_page'),
     path('mainpage/input/', views.topic, name='topic'),
     path('schedule/', views.schedule, name='design_schedule'),
-    path('schedule/data1', views.empData, name = 'emp_date_data'),
+    path('schedule/data1', views.empData, name='emp_date_data'),
+    path('schedule/data_schedule_prev', views.emp_schedule, name='emp_schedule_data'),
+    path('schedule/add_data', views.emp_save_data, name='save_schedule_data'),
+    path('schedule/delete_schedule', views.delete_schedule, name='delete_schedule_data'),
 ]
--- ScheduleProject/firstapp/views.py
+++ ScheduleProject/firstapp/views.py
@@ -1,8 +1,9 @@
+from django.db.models import Max
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
 # Create your views here.
-from firstapp.models import DimEmployee
+from firstapp.models import DimEmployee, TimeTable, Schedule, DimDate
 
 
 def schedulepage(request):
@@ -22,17 +23,60 @@ def topic(request):
 
 
 def schedule(request):
-    data = DimEmployee.objects.raw(''' Select * from firstapp_DimEmployee where Status = 'Doctor' ''')
-    lst = [{"id": entry.empId, "firstName":entry.firstName, "lastName":entry.lastName, "job":entry.status} for entry in data]
-    print(lst)
-    return render(request, 'firstapp/scheduleTable.html')
+    time = TimeTable.objects.raw(''' Select timeId, category from firstapp_TimeTable''')
+    time_lst = [{"timeId": entry.timeId, "category":entry.category} for entry in time]
+    #print(time_lst)
+    return render(request, 'firstapp/scheduleTable.html', {'time_slot': time_lst})
 
 
 def empData(request):
     empId = request.GET.get('empId', None)
-    print(empId)
     data = DimEmployee.objects.raw(''' Select * from firstapp_DimEmployee where EmpId = %s ''', [empId])
     lst = [{"id": entry.empId, "firstName":entry.firstName, "lastName":entry.lastName, "job":entry.status} for entry in data]
-    print(lst)
     send_data = json.dumps(lst)
     return HttpResponse(send_data)
+
+
+def emp_schedule(request):
+    empId = request.GET.get('empId', None)
+    data1 = DimEmployee.objects.get(empId=empId)
+    print(data1)
+    data = Schedule.objects.filter(employee=data1)
+    lst = [{"Id": str(d.scheduleId),
+            "date_full": str(d.keyDate.FullDate),
+            "shift": str(d.shift.category),
+            "start_time": str(d.shift.startTime),
+            "end_time": str(d.shift.endTime)}
+           for d in data]
+
+    send_data = json.dumps(lst)
+    return HttpResponse(send_data)
+
+
+def emp_save_data(request):
+    empId = request.GET.get('empId', None)
+    date = request.GET.get('date', None)
+    time_slot = request.GET.get('time_slot', None)
+    emp_data = DimEmployee.objects.get(empId=empId)
+    date_data = DimDate.objects.get(FullDate=date)
+    time_data = TimeTable.objects.get(timeId=time_slot)
+    check_data = Schedule.objects.filter(employee=emp_data, keyDate=date_data, shift=time_data)
+    if check_data.count() == 0:
+        terms = Schedule.objects.aggregate(Max('scheduleId'))
+        count = int(terms['scheduleId__max']) + 1
+        print(count)
+        n = Schedule(scheduleId=count, keyDate=date_data, employee=emp_data, shift=time_data)
+        n.save()
+        lst = ["Done"]
+    else:
+        lst = ["Undone"]
+
+    return HttpResponse(json.dumps(lst))
+
+
+def delete_schedule(request):
+    id_list = request.GET.get('list', None);
+    id_list = json.loads(id_list)
+    for id in id_list:
+        Schedule.objects.filter(scheduleId=id).delete()
+    return HttpResponse("")
